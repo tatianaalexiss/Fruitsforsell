59357450a288062c107dea97fc66561ba86a7287
tatianaalexiss <128632487+tatianaalexiss@users.noreply.github.com>
2023-05-01 14:33:02 -0400

    Update DPH.in.php

--- DPH.in.php
+++ DPH.in.php
@@ -9,3 +9,12 @@ $dbname = "fruit for sell";
 $conn = mysqli_connect ($dbservername, $dbusername, $dbpassword, $dbname);
 
 
+
+if (isset($_GET['search'])) {
+  $search = $_GET['search'];
+  // TODO: process the search query
+  echo "You searched for: " . $search;
+}
+
+
+
