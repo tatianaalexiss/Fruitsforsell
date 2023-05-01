9be7f767641897fd3497e976c627d09a891d862e
tatianaalexiss <128632487+tatianaalexiss@users.noreply.github.com>
2023-05-01 19:45:59 -0400

    Update DPH.in.php

--- DPH.in.php
+++ DPH.in.php
@@ -14,27 +14,6 @@ if (!$conn) {
 
 $result = mysqli_query($conn, "SELECT * FROM your_table");
 
-?>
 
-<table>
-  <thead>
-    <tr>
-      <th>ID</th>
-      <th>Name</th>
-      <th>Email</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php while ($row = mysqli_fetch_assoc($result)) { ?>
-      <tr>
-        <td><?php echo $row['id']; ?></td>
-        <td><?php echo $row['name']; ?></td>
-        <td><?php echo $row['email']; ?></td>
-      </tr>
-    <?php } ?>
-  </tbody>
-</table>
-
-<?php
 mysqli_close($conn);
 ?>
