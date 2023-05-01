1261aabd5d6e0d3f93885631a3613e20c8c462bf
tatianaalexiss <128632487+tatianaalexiss@users.noreply.github.com>
2023-05-01 15:20:13 -0400

    Update DPH.in.php

--- DPH.in.php
+++ DPH.in.php
@@ -1,20 +1,23 @@
 <?php
 
-$dbservername = "localhost";
-$dbusername = "root";
-$dbpassword = "";
+
+$servername = "localhost";
+$username = "root@localhost";
+$password = "NO";
 $dbname = "fruit for sell";
 
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-$conn = mysqli_connect ($dbservername, $dbusername, $dbpassword, $dbname);
-
-
-
-if (isset($_GET['search'])) {
-  $search = $_GET['search'];
-  // TODO: process the search query
-  echo "You searched for: " . $search;
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+
+
+$sql_file = file_get_contents("https://raw.githubusercontent.com/tatianaalexiss/Fruitsforsell/main/127_0_0_1.sql");
+$conn->multi_query($sql_file);
+
+
+$conn->close();
 
 
 
