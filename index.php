2e8899c2fcf15dd1d65bbb1544c60895f4baf642
tatianaalexiss <128632487+tatianaalexiss@users.noreply.github.com>
2023-05-01 13:40:19 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,19 +4,76 @@
 
 <!DOCTYPE html>
 <html>
-    <head>  
-        <style>
-      body {
-        background-color: black;
-        color: white;
-      }
-    </style>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {box-sizing: border-box;}
+
+body { 
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.header {
+  overflow: hidden;
+  background-color: white;
+  padding: 20px 10px;
+}
+
+.header a {
+  float: left;
+  color: black;
+  text-align: center;
+  padding: 12px;
+  text-decoration: none;
+  font-size: 18px; 
+  line-height: 25px;
+  border-radius: 4px;
+}
+
+.header a.logo {
+  font-size: 25px;
+  font-weight: bold;
+}
+
+.header a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.header a.active {
+  background-color: dodgerblue;
+  color: white;
+}
+
+.header-right {
+  float: right;
+}
+
+@media screen and (max-width: 500px) {
+  .header a {
+    float: none;
+    display: block;
+    text-align: left;
+  }
+  
+  .header-right {
+    float: none;
+  }
+}
+</style>	
+</head>
+<body>
+
+<div class="header">
+  <a href="#default" class="logo"> Exotic Fruits </a>
+  <div class="header-right">
+    <a href= "fruit.html">Fruits</a>
+    <a href="orders.html">Orders</a>
+  </div>
+</div>
+<style>
   </head>
-    <h1>
-        <center>
-        Exotic Fruits
-        </center>
-    </h1>
+   
     <body>
 $conn;
     </body>
