d7d0e1503ce73ce31ed8484f2e93e28b8b17e457
tatianaalexiss <128632487+tatianaalexiss@users.noreply.github.com>
2023-05-01 13:12:19 -0400

    Add files via upload

---
+++ DPH.in.php
@@ -0,0 +1,11 @@
+<?php
+
+$dbservername = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "fruit for sell";
+
+
+$conn = mysqli_connect ($dbservername, $dbusername, $dbpassword, $dbname);
+
+
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    include_once 'includes/DPH.in.php'  
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>  
+        <title>
+        Exotic Fruit For Sell
+        </title>
+    </head>
+    <body>
+$conn;
+    </body>
+</html>
