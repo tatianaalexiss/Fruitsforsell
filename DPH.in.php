844e5c2a748dbae917c742e36f0dc95851c8187d
tatianaalexiss <128632487+tatianaalexiss@users.noreply.github.com>
2023-05-01 15:45:41 -0400

    Update DPH.in.php

--- DPH.in.php
+++ DPH.in.php
@@ -6,18 +6,19 @@ $username = "root@localhost";
 $password = "NO";
 $dbname = "fruit for sell";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = mysqli_connect($hostname, $username, $password, $dbname);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
 }
 
+$sql_queries = $_POST['sql_queries'];
 
-$sql_file = file_get_contents("https://raw.githubusercontent.com/tatianaalexiss/Fruitsforsell/main/127_0_0_1.sql");
-$conn->multi_query($sql_file);
-
-
-$conn->close();
-
+if (mysqli_multi_query($conn, $sql_queries)) {
+  echo "SQL queries executed successfully";
+} else {
+  echo "Error executing SQL queries: " . mysqli_error($conn);
+}
+?>
 
 
