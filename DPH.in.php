e7c6c1cfdf78de77d3dea0ba4487e3c62e14b24e
tatianaalexiss <128632487+tatianaalexiss@users.noreply.github.com>
2023-05-01 20:12:23 -0400

    Update DPH.in.php

--- DPH.in.php
+++ DPH.in.php
@@ -13,6 +13,8 @@ if (!$conn) {
 }
 
 $result = mysqli_query($conn, "SELECT * FROM exotics_fruit_inventory ORDER BY fruit_name ASC;");
+$result = mysqli_query($conn, "SELECT `orders`.`order_id`, `order_details`.`fruit_id`, `exotics_fruit_inventory`.`fruit_name` FROM `orders` LEFT JOIN `order_details` ON `order_details`.`order_id` = `orders`.`order_id` LEFT JOIN `exotics_fruit_inventory` ON `order_details`.`fruit_id` = `exotics_fruit_inventory`.`fruit_id` WHERE `exotics_fruit_inventory`.`fruit_name` = 'pineapple';");
+
 
 
 mysqli_close($conn);
